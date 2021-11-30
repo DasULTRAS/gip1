9fdaeaf058014a8aaa6da01ea283ff51b3edf42f
Christian Hoelter <hoelter.christian@gmx.de>
2021-11-30 21:03:45 +0100

    Scan & Print eines Labyrinth

--- 06-modularisierung/practical/main.c
+++ 06-modularisierung/practical/main.c
@@ -4,33 +4,32 @@
 
 char labyrinth[MAZE_SIZE +1][MAZE_SIZE +1];
 
-void scan_labyrinth(){
-	char temp;
-	char input;
-	for(int i = 0; i < MAZE_SIZE; ++i){
-		for(int j = 0; j < MAZE_SIZE; ++j){
-			intput = scanf("%[#!@ ]%*[\n]", &temp);
-			if (input == EOF)
-				return;
-
-			printf("%c\n", temp);
-			labyrinth[i][j] = temp;
-			/*
-			scanf("%c", &temp);
-			if(temp == '#' || temp == '!' || temp == '@' || temp == ' ')
-				labyrinth[i][j] = temp;
-			else if(temp == '\n' && j == MAZE_SIZE-1){
-				labyrinth[i][j] = '\0';
-			}
-			*/
-		}
-	}
-}
-
 void print_labyrinth(){
 	for(int i = 0; i < MAZE_SIZE; ++i){
 			printf("%s\n", labyrinth[i]);
 	}
+}
+
+void scan_labyrinth(){
+	int input;
+	int zeile = 0;
+	while (1) {
+		input = scanf("%[#!@ ]%*[\n] ", labyrinth[zeile]);
+
+		if(input == EOF)
+			break;
+
+		++zeile;
+	}
+}
+
+/**
+bool solve_maze(unsigned int x, unsigned int y), die versucht,
+einen Weg von einem durch x und y angegeben Startpunk
+(markiert durch @) zum Ziel (markiert durch !) zu finden
+*/
+bool solve_maze(unsigned int x, unsigned int y){
+	
 }
 
 int main(){
