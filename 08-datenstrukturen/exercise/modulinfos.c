e5fd3fd2d65c34c02c62d92c687a0c0cca406d24
Christian Hoelter <hoelter.christian@gmx.de>
2021-11-30 14:38:46 +0100

    print_modul und print_format(Zeilen und Zeichen kennzeichnung)

--- 08-datenstrukturen/exercise/modulinfos.c
+++ 08-datenstrukturen/exercise/modulinfos.c
@@ -6,6 +6,30 @@ struct modul {
   unsigned int workload, credits, semester, dauer;
 };
 
+void print_format(int zeilen){
+  for(int i = 0;  i < zeilen; ++i){
+    printf("%d", i);
+    for(int j = 0; j < 9; ++j)
+      printf(" ");
+  }
+  printf("\n");
+
+  for(int i = 0;  i < zeilen; ++i){
+    for(int j = 0; j < 10; ++j)
+      printf("%d", j);
+  }
+  printf("\n");
+}
+
+void print_modul(struct modul modul){
+  printf("%s\nKürzel  : %-5s     Workload: %-5u     Credits : %u\nSemester: %-5u     Dauer   : %u\n", modul.titel, modul.abkuerzung, modul.workload, modul.credits, modul.semester, modul.dauer);
+}
+
 int main(){
+  struct modul modulinfos = {"Grundlagen der Informatik und Programmierung 1", "GIP1", 210, 7, 1, 1};
+
+  print_format(6);
+  print_modul(modulinfos);
+
   return 0;
 }
