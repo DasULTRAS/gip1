99684ee941b6e69b6c3fa0cd79cf864f30554648
Christian Hoelter <hoelter.christian@gmx.de>
2021-11-30 12:10:50 +0100

    Try scanf only used chars;

--- 06-modularisierung/practical/main.c
+++ 06-modularisierung/practical/main.c
@@ -6,14 +6,23 @@ char labyrinth[MAZE_SIZE +1][MAZE_SIZE +1];
 
 void scan_labyrinth(){
 	char temp;
+	char input;
 	for(int i = 0; i < MAZE_SIZE; ++i){
 		for(int j = 0; j < MAZE_SIZE; ++j){
+			intput = scanf("%[#!@ ]%*[\n]", &temp);
+			if (input == EOF)
+				return;
+
+			printf("%c\n", temp);
+			labyrinth[i][j] = temp;
+			/*
 			scanf("%c", &temp);
 			if(temp == '#' || temp == '!' || temp == '@' || temp == ' ')
 				labyrinth[i][j] = temp;
 			else if(temp == '\n' && j == MAZE_SIZE-1){
 				labyrinth[i][j] = '\0';
 			}
+			*/
 		}
 	}
 }
@@ -23,13 +32,6 @@ void print_labyrinth(){
 			printf("%s\n", labyrinth[i]);
 	}
 }
-/*
-void print_labyrinth(){
-	for(int i = 0; i < MAZE_SIZE; ++i){
-		printf("%s\n", labyrinth[i]);
-	}
-}
-*/
 
 int main(){
 	scan_labyrinth();
