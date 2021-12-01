6582cd5ca08f122b0f3536050345c311a56d68e2
DasULTRAS <kitty16092012@gmail.com>
2021-12-01 15:47:40 +0100

    Rekursive solve of maze. Markieren des Weges mit Pfeilen.

--- 06-modularisierung/practical/main.c
+++ 06-modularisierung/practical/main.c
@@ -49,15 +49,34 @@ einen Weg von einem durch x und y angegeben Startpunk
 bool solve_maze(unsigned int x, unsigned int y){
 		if(maze[y][x] == '!') {
 			return true;
-		} else if (maze[y][x] != '#'){
-
-		} else if (maze[y][x] != '#'){
-
-		} else if (maze[y][x] != '#'){
-
-		} else if (maze[y][x] != '#'){
-
+		} else if(maze[y][x] == '#'){
+			return false;
 		}
+
+		maze[y][x] = '#';
+
+		// Nach unten
+		if(solve_maze(x, y+1)){
+			maze[y][x] = 'v';
+			return true;
+		}
+		// Nach rechts
+		if(solve_maze(x+1, y)){
+			maze[y][x] = '>';
+			return true;
+		}
+		// Nach links
+		if(solve_maze(x-1, y)){
+			maze[y][x] = '<';
+			return true;
+		}
+		// Nach oben
+		if(solve_maze(x, y-1)){
+			maze[y][x] = '^';
+			return true;
+		}
+
+		maze[y][x] = '*';
 		return false;
 }
 
@@ -65,11 +84,13 @@ int main(){
 	scan_maze();
 	print_maze();
 
-	if(solve_maze(19, 20)){
-		printf("labyrinth gelöst.");
+	if(solve_maze(1, 1)){
+		printf("\nMaze solved.\n");
 	} else {
-		printf("labyrinth nicht gelöst.");
+		printf("\nMaze not solved.\n");
 	}
+
+	print_maze();
 
 	return 0;
 }
