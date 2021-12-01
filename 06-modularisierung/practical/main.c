254a447c42ebfaef352d527b3dd3f5d1907f1366
Christian Hoelter <hoelter.christian@gmx.de>
2021-12-01 14:05:49 +0100

    solve_maze erkennt ob das Labyrinth gelöst wurde und labyrinth wurde in Maze umbenannt

--- 06-modularisierung/practical/main.c
+++ 06-modularisierung/practical/main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 
 #define MAZE_SIZE 22
 
-char labyrinth[MAZE_SIZE +1][MAZE_SIZE +1];
+char maze[MAZE_SIZE +1][MAZE_SIZE +1];
 
-void print_labyrinth(){
+void print_maze(){
+	// Koordniaten
+	printf("\n    ");
+	for(int i = 0;  i < MAZE_SIZE/10+1; ++i){
+    printf("%d", i);
+    for(int j = 0; j < 9; ++j)
+      printf(" ");
+  }
+
+	printf("\n    ");
+	for(int i = 0;  i < MAZE_SIZE/10+1; ++i){
+    for(int j = 0; j < 10; ++j)
+      printf("%d", j);
+  }
+	printf("\n");
+
+	// labyrinth
 	for(int i = 0; i < MAZE_SIZE; ++i){
-			printf("%s\n", labyrinth[i]);
+			printf("%-3d-%s\n", i, maze[i]);
 	}
 }
 
-void scan_labyrinth(){
+void scan_maze(){
 	int input;
 	int zeile = 0;
 	while (1) {
-		input = scanf("%[#!@ ]%*[\n] ", labyrinth[zeile]);
+		input = scanf("%[#!@ ]%*[\n] ", maze[zeile]);
 
 		if(input == EOF)
 			break;
@@ -29,12 +47,15 @@ einen Weg von einem durch x und y angegeben Startpunk
 (markiert durch @) zum Ziel (markiert durch !) zu finden
 */
 bool solve_maze(unsigned int x, unsigned int y){
-	
+		if(maze[y][x] == '!')
+			return true;
+		return false;
 }
 
 int main(){
-	scan_labyrinth();
-	print_labyrinth();
-
+	scan_maze();
+	print_maze();
+	
+	assert(solve_maze(19, 20));
 	return 0;
 }
