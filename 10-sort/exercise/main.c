4a09ca846854756fe8ec50dbb9c682dc4b80675f
DasULTRAS <kitty16092012@gmail.com>
2021-12-21 12:25:21 +0100

    debugging

--- 10-sort/exercise/main.c
+++ 10-sort/exercise/main.c
@@ -34,16 +34,18 @@ int main() {
     // use current time as seed for random generator
     srand(time(NULL));
 
-    int *int_array = get_random_int_array(length);
-    print_int_array(int_array, length);
-
-    assert(is_sorting(1000, 1000, &bubble_sort));
-    assert(is_sorting(1000, 1000, &selection_sort));
-    assert(is_sorting(1000, 1000, &insertion_sort));
+    int* int_array_1 = get_random_int_array(10);
+    print_int_array(int_array_1, 10);
+    selection_sort(int_array_1, 10);
+    print_int_array(int_array_1, 10);
 
     // Reservierten Speicher zurückgeben
-    free(int_array);
-    int_array = NULL;
+    free(int_array_1);
+    int_array_1 = NULL;
+
+    assert(is_sorting(100, 100, &bubble_sort));
+    assert(is_sorting(100, 100, &selection_sort));
+    assert(is_sorting(100, 100, &insertion_sort));
 
     return 0;
 }
--- 10-sort/exercise/my_sorting/my_sorting.c
+++ 10-sort/exercise/my_sorting/my_sorting.c
@@ -11,15 +11,15 @@ b = temp;\
  * @param length
  */
 void bubble_sort(int *array, unsigned int length) {
-    for (int i = length; i > 0; --i)
+    for (int i = length-1; i > 0; --i)
         for (int j = 0; j < i; ++j)
             if (array[j] > array[j + 1])
                 swap(array[j], array[j + 1]);
 }
 
 void selection_sort(int *array, unsigned int length){
-    for(int i = 0; i < length; ++i){
-        int smallest = array[i];
+    for(int i = 0; i < (length-1); ++i){
+        int smallest = i;
         for(int j = i+1; j < length; ++j)
             if(array[smallest] > array[j])
                 smallest = j;
@@ -28,7 +28,7 @@ void selection_sort(int *array, unsigned int length){
 }
 
 void insertion_sort(int *array, unsigned int length){
-    for(int i = 1; i < length; ++length)
+    for(int i = 1; i < length; ++i)
         for(int j = i; j > 0 && array[j-1] > array[j]; --j)
             swap(array[j], array[j-1]);
 }
