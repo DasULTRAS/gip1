528601f3f85e97baed3dd60eddb2df17c0d77905
DasULTRAS <kitty16092012@gmail.com>
2021-12-21 15:52:45 +0100

    init

--- 11-trees/practical/main.c
+++ 11-trees/practical/main.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "src/modul.h"
+
 int main(){
-    printf("Hello World!");
+    struct modul gip1 = {"Grundlagen der Informatik und Programmierung 1", "GIP1", 210, 7, 1, 1};
+
+    print_modul(gip1);
 }
