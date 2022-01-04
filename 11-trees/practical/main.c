41b4dca4109cb9598a026f273e3d9a1633a4a302
Christian Hoelter <hoelter.christian@gmx.de>
2022-01-04 17:38:03 +0100

    mehrere Module aufnehmen

--- 11-trees/practical/main.c
+++ 11-trees/practical/main.c
@@ -18,13 +18,19 @@ int modul_cmp(struct modul first, struct modul second){
 }
 
 int main(){
-    struct modul gip1 = {"Grundlagen der Informatik und Programmierung 1", "GIP1", 210, 7, 1, 1};
-    struct modul gip2 = {"Grundlagen der Informatik und Programmierung 2", "GIP2", 210, 7, 2, 1};
+    // struct modul gip1 = {"Grundlagen der Informatik und Programmierung 1", "GIP1", 210, 7, 1, 1};
+    // struct modul gip2 = {"Grundlagen der Informatik und Programmierung 2", "GIP2", 210, 7, 2, 1};
 
     struct modul_tree* tree = construct_modul_tree(&modul_cmp);
-    insert_sorted_tree(tree, gip1);
-    insert_sorted_tree(tree, gip2);
 
-    print_modul(tree->first->data);
-    print_modul(tree->first->right->data);
+    int input;
+    while (1) {
+        struct modul temp = {"", "", 0, 0, 0, 0};
+        input = scanf("%64[^,],%6[^,],%u,%u,%u,%u ", temp.titel, temp.abkuerzung, &temp.workload, &temp.credits, &temp.semester, &temp.dauer);
+        if(input == EOF)
+            break;
+        insert_sorted_tree(tree, temp);
+    }
+
+    printf("FERTIG");
 }
